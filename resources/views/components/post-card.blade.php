f0ad041dfe6212f7a1ee3739e96be60c503a52f0
J-Skarbek <john@snarkmediaagency.com>
2021-10-09 19:19:53 -0500

    updating 10/9

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -51,7 +51,7 @@
                     </div>
 
                     <div>
-                        <a href="/categories/{{ $post->category->slug }}"
+                        <a href="/posts/{{ $post->slug }}"
                            class="transition-colors duration-300 text-xs font-semibold bg-gray-200 hover:bg-gray-300 rounded-full py-2 px-8"
                         >
                             Read More
