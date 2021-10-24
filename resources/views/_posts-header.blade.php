675b6ccca738753fa0985ecc80f91d3265f83293
J-Skarbek <john@snarkmediaagency.com>
2021-10-24 11:43:04 -0500

    wrapping up lesson 34

--- resources/views/_posts-header.blade.php
+++ resources/views/_posts-header.blade.php
@@ -22,7 +22,7 @@
                     @click="show = ! show" 
                     class="py-2 pl-3 pr-9 test-xs font-semibold w-full lg:w-32 text-left flex lg:inline-flex"
                     >
-                        Categories
+                        {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
 
                         <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
                             height="22" viewBox="0 0 22 22">
@@ -36,13 +36,20 @@
                 </button>
 
                 <div x-show="show" class="py-2 absolute bg-gray-100 w-full mt-2 rounded-xl w-full z-50" style="display:none">
+                    <a href="/" 
+                        class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white"
+                        >All</a>
+
                     @foreach ($categories as $category)
                     <a href="/categories/{{ $category->slug }}" 
-                        class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white"
+                        class="
+                        block text-left px-3 text-sm leading-6 
+                        hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white
+                        {{ isset($currentCategory) && $currentCategory->is($category) ? 'bg-blue-500 text-white' : '' }}
+                        "
                         >{{ ucwords($category->name) }}</a>
-                    @endforeach                
-                    {{-- <a class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white" href="#">Two</a>
-                    <a class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white" href="#">Three</a> --}}
+                    @endforeach             
+
                 </div>
             </div>
         </div>
